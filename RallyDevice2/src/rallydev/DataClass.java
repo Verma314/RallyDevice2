8e317d5717a524f5393e6b9095a52c7922e8d299
Verma314 <adityasinghverma314@gmail.com>
2016-10-16 03:59:49 +0530

    Add files via upload

---
+++ RallyDevice2/src/rallydev/DataClass.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rallydev;
+
+/**
+ *
+ * @author MAHE
+ */
+public class DataClass {
+    public
+    int startDistance;
+    int endDistance;
+    int tulipDistance;
+    int averageSpeed;
+    int tulipNumber;
+    int idealTime;
+    
+    //TODO:
+    //write further datafields for the GPS class as well
+}
